721fe0be1e014707cfc8abbd33fba1817131f86d
VV1nc3nt <gormmyz@gmail.com>
2023-04-24 23:16:02 +0300

    fix

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,14 @@
+/* eslint-disable max-len */
+/* eslint-disable no-bitwise */
 export default class Validator {
   validateUsername(username) {
-    this.regexTestTemplate = /^([^\W\d_])+(([A-z])|(\d){0,3}|(_)|(-))+([^\W\d_])$/.test(username);
-    return this.regexTestTemplate;
+    this.rowStartOnlyLetters = /^([^\W\d_])/.test(username);
+    this.allowedLetters = /([A-z])/.test(username);
+    this.allowedThreeDigits = /(\d){0,3}/.test(username);
+    this.allowedUnderscores = /(_)/.test(username);
+    this.allowedDashes = /(-)/.test(username);
+    this.rowEndOnlyLetters = /([^\W\d_]$)/.test(username);
+
+    return this.rowStartOnlyLetters && this.allowedLetters | this.allowedThreeDigits | this.allowedUnderscores | this.allowedDashes && this.rowEndOnlyLetters;
   }
 }
